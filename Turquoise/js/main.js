edf297d9c43bd2cda54f4667704d64da10220f27
DRN27 <47753504+DRN27@users.noreply.github.com>
2019-06-02 08:48:56 +0200

    Add files via upload

---
+++ Turquoise/js/main.js
@@ -0,0 +1,22 @@
+// Отправка заявки 
+$(document).ready(function() {
+	$('#form').submit(function() { // проверка на пустоту заполненных полей. Атрибут html5 — required не подходит (не поддерживается Safari)
+		if (document.form.name.value == '' || document.form.phone.value == '' || document.form.text.value == '' ) {
+			valid = false;
+			return valid;
+		}
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			alert("cool");
+			$(this).find('input').val('');
+			$(this).find('textarea').val('');
+			$('#form').trigger('reset');
+		});
+		return false;
+	});
+});
+
+
