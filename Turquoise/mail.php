af3d32322e4038929894838a61551cd9d189d745
DRN27 <47753504+DRN27@users.noreply.github.com>
2019-06-02 08:42:42 +0200

    Add files via upload

---
+++ Turquoise/mail.php
@@ -0,0 +1,14 @@
+<?php
+
+$recepient = "mdorin101@gmail.com";
+$siteName = "Turquoise";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$text = trim($_POST["text"]);
+$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+
+$pagetitle = "Заявка с сайта \"$siteName\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+?>
